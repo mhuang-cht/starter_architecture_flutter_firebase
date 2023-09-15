807ba497f7f049de75e2715f932ef5e149a8a441
mhuang <mhuang@carbonite.com>
2023-09-14 22:05:15 -0400

    Fix change-app-name.sh

--- change-app-name.sh
+++ change-app-name.sh
@@ -2,23 +2,35 @@
 
 app_name="$1"
 old_name="starter_architecture_flutter_firebase"
+
 old_package_name="com.example.$old_name"
 new_package_name="com.chtsolutions.$app_name"
+
+app_name_camel=$(sed -r 's/(_)(\w)/\U\2/g' <<<"$app_name")
+old_name_camel=$(sed -r 's/(_)(\w)/\U\2/g' <<<"$old_name")
+old_package_name_camel="com.example.$old_name_camel"
+new_package_name_camel="com.chtsolutions.$app_name_camel"
 
 if [ -z "$app_name" ]; then
     echo Usage: $(basename $0) app_name
     exit 1
 fi
 
-invalid_name=$(echo $app_name | grep [^a-z_])
+invalid_name=$(echo $app_name | grep [^a-z0-9_])
 if [ -n "$invalid_name" ]; then
     echo "Invalid app name \"$app_name\""
-    echo "The name must start with lower case letter, and only contain lower case letter and underscore (_)"
+    echo "The name must start with lower case letter, and only contain lower case letter, numbers and underscore (_)"
     exit 1
 fi
 
-grep -rl $old_package_name . | grep -v "^./.git/" | xargs -i@ sed -i "s/$old_package_name/$new_package_name/g" @
-grep -rl $old_name . | grep -v "^./.git/" | xargs -i@ sed -i "s/$old_name/$app_name/g" @
+grep -rl $old_package_name . | grep -v "^./.git/" | grep -v "change-app-name.sh" | xargs -i@ sed -i "s/$old_package_name/$new_package_name/g" @
+grep -rl $old_package_name_camel . | grep -v "^./.git/" | grep -v "change-app-name.sh" | xargs -i@ sed -i "s/$old_package_name_camel/$new_package_name_camel/g" @
+grep -rl $old_name . | grep -v "^./.git/" | grep -v "change-app-name.sh" | xargs -i@ sed -i "s/$old_name/$app_name/g" @
+grep -rl $old_name_camel . | grep -v "^./.git/" | grep -v "change-app-name.sh" | xargs -i@ sed -i "s/$old_name_camel/$app_name_camel/g" @
+
+# remove platforms not needed
+rm -fr macos web
+
 sed -i 's/starter-architecture-flutter/cht-debug/g' .firebaserc
 set -x
-firebase login && flutterfire configure
+firebase login && flutterfire.bat configure --yes 
